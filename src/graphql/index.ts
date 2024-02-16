b4d3481ef06bc02431381da959aaed5133d53efd
nagarmanoj <nagar.manojkumar1999@gmail.com>
2024-02-16 17:37:26 +0530

    Prism Set up and Authorization

---
+++ src/graphql/index.ts
@@ -0,0 +1,29 @@
+import { ApolloServer } from '@apollo/server';
+import { User } from './user'
+async function createApolloGraphqlServer(){
+    const gqlServer = new ApolloServer({
+        typeDefs: `
+            ${User.typeDefs}
+            type Query{
+                ${User.queries}
+            }
+            type Mutation {
+                ${User.mutations}
+            }
+        `,
+        resolvers:{
+            Query:{
+                ...User.resolvers.queries
+            },
+            Mutation:{
+                ...User.resolvers.mutations
+            },
+        },
+    });
+    //Start gql server
+    await gqlServer.start();
+
+    return gqlServer;
+}
+
+export default createApolloGraphqlServer;
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,33 @@
 import express from "express";
-import { ApolloServer } from '@apollo/server';
+import createApolloGraphqlServer from "./graphql";
 import { expressMiddleware } from '@apollo/server/express4';
+import UserService from "./services/user";
+
 
 async function init() {
     
     const app = express();
     const PORT = Number(process.env.PORT) || 8000;
 
-    app.use(express.json());
-
-    //Create Graphql server
-    const gqlServer = new ApolloServer({
-        typeDefs: `
-            type Query{
-                hello:String
-            }
-        `,
-        resolvers:{
-            Query:{
-                hello:()=> `hi there i am nagar`
-            }
-        },
-    });
-    //Start gql server
-    await gqlServer.start();
+    app.use(express.json()); 
+    
 
     app.get("/",(req,res)=>{
         res.json({message:"server is up and rinning"});
     })
-    app.use("/graphql",expressMiddleware(gqlServer));
+    
+    app.use("/graphql",expressMiddleware(await createApolloGraphqlServer(),{
+        context:async({req})=>{
+            const token = req.headers.authorization
+            //console.log(token);
+            try{
+                const user = UserService.decodeJWTToken(token as string);
+                return { user };
+            }catch(err){
+                return {};
+            }
+        }
+    }));
     app.listen(PORT,()=>console.log(`Server started at port:${PORT}`));
 }
 init();
